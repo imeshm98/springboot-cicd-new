cbf0fe1607dea0eb389d7d68160783414a8acb80
Imesh Madusanka <imeshmofficial@gmail.com>
2022-12-02 14:05:36 +0530

    First commit

---
+++ src/main/java/com/groupone/springbootappdemo/SpringbootAppDemoApplication.java
@@ -0,0 +1,13 @@
+package com.groupone.springbootappdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAppDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAppDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/groupone/springbootappdemo/SpringbootAppDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupone.springbootappdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAppDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
